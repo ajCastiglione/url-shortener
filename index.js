f85002ac91180befc90a19f338f1d938596750bc
Antonio Castiglione <antoniocastiglione10@gmail.com>
2019-08-02 11:29:33 -0400

    updated the request url since the /api was unnecessary, also added the word port to cl

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.json({ extended: false }));
 
 // Define routes
 app.use("/", require("./routes/index"));
-app.use("/api/url", require("./routes/url"));
+app.use("/url", require("./routes/url"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
